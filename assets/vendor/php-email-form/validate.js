9feb1b01cb3a3d4bc0361a3a83c2a9da52e026d9
am-goku <ankrishna2483@gmail.com>
2022-12-11 20:14:12 +0530

    final

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -17,10 +17,10 @@
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
+      // if( ! action ) {
+      //   displayError(thisForm, 'The form action property is not set!')
+      //   return;
+      // }
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
@@ -63,13 +63,16 @@
       if (data.trim() == 'OK') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
+    }) 
+    //else
+    //   {
+    //     throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+    //   }
+    // })
+    // .catch((error) => {
+    //   displayError(thisForm, error);
+    // });
   }
 
   function displayError(thisForm, error) {
